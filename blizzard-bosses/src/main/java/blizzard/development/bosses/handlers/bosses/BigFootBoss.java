304baa50cc48d1a4afcb6e4ee73c9c8832dea05e
LucwsH <lucashtuber5@gmail.com>
2024-11-22 17:23:37 -0300

    feat: too many shits

--- blizzard-bosses/src/main/java/blizzard/development/bosses/handlers/bosses/BigFootBoss.java
+++ blizzard-bosses/src/main/java/blizzard/development/bosses/handlers/bosses/BigFootBoss.java
@@ -1,7 +1,6 @@
 package blizzard.development.bosses.handlers.bosses;
 
 import blizzard.development.bosses.enums.Bosses;
-import blizzard.development.bosses.utils.packets.npcs.NPC;
 import com.comphenix.protocol.ProtocolLibrary;
 import org.bukkit.entity.Player;
 
@@ -10,15 +9,12 @@ import java.util.UUID;
 public class BigFootBoss {
     public static void spawn(Player player) {
         UUID uuid = UUID.randomUUID();
-        NPC npc = new NPC(ProtocolLibrary.getProtocolManager(), uuid);
 
         Bosses boss = Bosses.BIGFOOT;
         String name = boss.getName();
         String value = boss.getValue();
         String signature = boss.getSignature();
 
-        npc.spawn(player, "§bPé Grande", value, signature, null);
-        npc.activateSecondLayer(player);
         player.setCompassTarget(null);
     }
 }
